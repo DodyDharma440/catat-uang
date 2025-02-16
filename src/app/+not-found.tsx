4d2ad1da82bacc382126eee6f366c6bc8b5218c4
Dodi Aditya <dodiaridharma@gmail.com>
2025-02-16 14:10:43 +0800

    chore: setup eslint

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,5 +1,6 @@
+import { Text } from "react-native";
+
 import { Stack } from "expo-router";
-import { StyleSheet, Text } from "react-native";
 
 export default function NotFoundScreen() {
   return (
@@ -9,16 +10,3 @@ export default function NotFoundScreen() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
